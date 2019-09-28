57d2bb3b66834ee8a4699f061717af3b8b8c9571
Elizabeth Bodzy <bodze087@newschool.edu>
2019-09-28 11:43:08 -0500

    fixed syntax bug

--- config/connection.js
+++ config/connection.js
@@ -4,13 +4,14 @@ var connection;
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else { 
- connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Yosterkitty3",
-  database: "burgers_db"
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Yosterkitty3",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
