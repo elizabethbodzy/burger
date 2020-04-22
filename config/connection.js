7aa435a8a7c790f02d4cd35e8684504a677914d2
Elizabeth Bodzy <bodze087@newschool.edu>
2020-04-22 16:27:30 -0500

    updated dependencies from github alert

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else { 
+} else {
   connection = mysql.createConnection({
-    host: "localhost",
+    host: "d6ybckq58s9ru745.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     port: 3306,
-    user: "root",
-    password: "Yosterkitty3",
-    database: "burgers_db"
+    user: "x2l27lpb9f6j2l8j",
+    password: "z3640j9vzc1w8egi",
+    database: "dbti6ixj9sk361n8"
   });
 }
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
